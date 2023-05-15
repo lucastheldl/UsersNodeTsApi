3af3fdeaaddfd838924ee284677481bb3c7a30fe
lucastheldl <lucastheldl@gmail.com>
2023-05-15 12:22:05 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello worl")
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/users", async (req, res) => {
+  try {
+    return res.status(201).send("hello");
+  } catch {
+    return res.status(500).send("erro");
+  }
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log("rodando"));
